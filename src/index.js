d55c3249125cb0a822d0f162759ba9438dceff35
Milivoje Vujadinović <milivoje.vujadinovic@gmail.com>
2022-07-13 20:16:32 +0200

    Rename application files

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import RiskFree from './RiskFree';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RiskFree />
   </React.StrictMode>
 );
 
