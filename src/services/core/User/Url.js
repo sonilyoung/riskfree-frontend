f7d0c374b31ab5c5869e96978024683dd5e23cc5
Milivoje Vujadinović <milivoje.vujadinovic@gmail.com>
2022-09-06 10:58:40 +0200

    Add more user services

---
+++ src/services/core/User/Url.js
@@ -0,0 +1,22 @@
+import { RoleService } from './';
+
+export const HOME_PAGE_ADMIN_PATH = '/dashboard/system-administrator';
+export const HOME_PAGE_DIRECTOR_PATH = '/dashboard/director';
+export const HOME_PAGE_EMPLOYEE_PATH = '/dashboard/employee';
+
+export const getHomePagePath = (roleCode) => {
+    switch (roleCode) {
+        case RoleService.ROLE_CODE_ADMIN:
+            return HOME_PAGE_ADMIN_PATH;
+            break;
+        case RoleService.ROLE_CODE_DIRECTOR:
+            return HOME_PAGE_DIRECTOR_PATH;
+            break;
+        case RoleService.ROLE_CODE_EMPLOYEE:
+            return HOME_PAGE_EMPLOYEE_PATH;
+            break;
+    }    
+
+    return false;
+};
+
