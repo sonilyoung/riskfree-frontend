d350d3c84669e4c7d9018b17e4b0667278a6140c
stevanlalovic <stevan.lalovic@mvsoft.rs>
2022-09-19 17:58:10 +0200

    Dialog added useStyles folder

---
+++ src/dialogs/Upload/useStyles.js
@@ -0,0 +1,83 @@
+import { makeStyles } from '@mui/styles';
+
+const useStyles = makeStyles(() => ({
+    uploadPopup: {
+        position: 'absolute',
+        zIndex: '1000',
+        top: '200px',
+        left: '50%',
+        transform: 'translateX(-50%)',
+        width: '400px',
+        height: '400px',
+        background: '#fff',
+        borderRadius: '30px',
+        padding: '40px',
+        boxSizing: 'border-box',
+        display: 'flex',
+        flexWrap: 'wrap',
+        // display: 'none !important',
+        '& >span': {
+            width: '20%',
+            height: '20px',
+            borderBottom: '1px solid #bdcbe9',
+            transform: 'translateY(-5px)',
+            '&:nth-of-type(2)': {
+                width: '60%',
+                border: 'none',
+                padding: '0 10px',
+                boxSizing: 'border-box',
+                textAlign: 'center',
+                transform: 'unset',
+            }
+        },
+        '& >button': {
+            position: 'absolute',
+            top: '0px',
+            right: '-65px'
+        }
+    },
+    uploadPopupClose: {
+        display: 'none !important',
+    },
+    uploadSearch: {
+        display: 'flex',
+        flexWrap: 'wrap',
+        justifyContent: 'center',
+        '& button:first-of-type': {
+            marginLeft: '10px'
+        }
+    },
+    uploadInfo: {
+        display: 'flex',
+        flexWrap: 'wrap',
+        justifyContent: 'center',
+        alignItems: 'flex-start',
+        height: '50%',
+        '& >*': {
+            width: '100%',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center'
+        },
+        '& img': {
+            width: '30px',
+            height: '30px',
+        }
+    },
+    popupTextField: {
+        marginBottom: '10px !important',
+        overflow: 'hidden',
+        height: '40px',
+        '& >div': {
+            background: '#fff',
+            fontSize: '16px',
+        },
+        '& input': {
+            fontSize: '16px',
+            height: '40px',
+            boxSizing: 'border-box',
+        }
+    },
+}))
+
+export { useStyles };
