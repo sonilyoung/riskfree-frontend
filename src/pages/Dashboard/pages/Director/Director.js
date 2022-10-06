66f7319a8c8ea9bf8bd96ce95cf38e723c98d3d5
손일영 <iys4906@naver.com>
2022-10-06 16:01:09 +0900

    20221006_jimmy_대표:실무_링크_edit_3

--- src/pages/Dashboard/pages/Director/Director.js
+++ src/pages/Dashboard/pages/Director/Director.js
@@ -435,15 +435,15 @@ const Director = () => {
     }
 
     //실무자 메인페이지로 이동
-    function DashboardLinkShow(MainKey) {
-        if(MainKey === "12") {
-            //navigate('/dashboard/employee/order-for-improvement-and-correction-under-related-law/list');
-            window.open(`${BASE_URL_FRONT}dashboard/employee/order-for-improvement-and-correction-under-related-law/list`, '_blank')
-        } else {
-            //navigate(`/dashboard/employee/${MainKey}`);
-            window.open(`${BASE_URL_FRONT}dashboard/employee/${MainKey}`, '_blank')
-        }
-    }
+    // function DashboardLinkShow(MainKey) {
+    //     if(MainKey === "12") {
+    //         //navigate('/dashboard/employee/order-for-improvement-and-correction-under-related-law/list');
+    //         //window.open(`dashboard/employee/order-for-improvement-and-correction-under-related-law/list`, '_blank')
+    //     } else {
+    //         //navigate(`/dashboard/employee/${MainKey}`);
+    //         //window.open(`dashboard/employee/${MainKey}`, '_blank')
+    //     }
+    // }
 
     const fetchBaselineList = async () => {
         const response = await getBaselineList({});
@@ -922,55 +922,55 @@ const Director = () => {
                     <Slider className={classes.dashSlider} {...dashboardSlider} >
                         <div className={classes.dashboardSlide}>
                             <div className={classes.slickCircle + handleSlickCircleColor(essentialRateList?.RET_DATA?.rate1?.score)}>
-                                <Link to="#" onClick={() => DashboardLinkShow('1')} className={classes.slickLink} underline="none">
+                                <Link to="dashboard/employee/1" className={classes.slickLink} underline="none">
                                     <div><strong>{essentialRateList?.RET_DATA?.rate1?.score ? essentialRateList?.RET_DATA?.rate1?.score : "0%"}</strong></div>
                                     <div>안전보건 목표 및<br /> 경영방침</div>
                                 </Link>
                             </div>
                             <div className={classes.slickCircle + handleSlickCircleColor(essentialRateList?.RET_DATA?.rate2?.score)}>
-                                <Link to="#" onClick={() => DashboardLinkShow('2')} className={classes.slickLink} underline="none">
+                                <Link to="dashboard/employee/2" className={classes.slickLink} underline="none">
                                     <div><strong>{essentialRateList?.RET_DATA?.rate2?.score ? essentialRateList?.RET_DATA?.rate2?.score : "0%"}</strong></div>
                                     <div>안전보건 총괄관리<br /> 전담조직</div>
                                 </Link>
                             </div>
                             <div className={classes.slickCircle + handleSlickCircleColor(essentialRateList?.RET_DATA?.rate3?.score)}>
-                                <Link to="#" onClick={() => DashboardLinkShow('3')} className={classes.slickLink} underline="none">
+                                <Link to="dashboard/employee/3" className={classes.slickLink} underline="none">
                                     <div><strong>{essentialRateList?.RET_DATA?.rate3?.score ? essentialRateList?.RET_DATA?.rate3?.score : "0%"}</strong></div>
                                     <div>유해요인개선<br /> 업무절차</div>
                                 </Link>
                             </div>
                             <div className={classes.slickCircle + handleSlickCircleColor(essentialRateList?.RET_DATA?.rate4?.score)}>
-                                <Link to="#" onClick={() => DashboardLinkShow('4')} className={classes.slickLink} underline="none">
+                                <Link to="dashboard/employee/4" className={classes.slickLink} underline="none">
                                     <div><strong>{essentialRateList?.RET_DATA?.rate4?.score ? essentialRateList?.RET_DATA?.rate4?.score : "0%"}</strong></div>
                                     <div>예산편성 및<br /> 집행관리</div>
                                 </Link>
                             </div>
                             <div className={classes.slickCircle + handleSlickCircleColor(essentialRateList?.RET_DATA?.rate5?.score)}>
-                                <Link to="#" onClick={() => DashboardLinkShow('5')} className={classes.slickLink} underline="none">
+                                <Link to="dashboard/employee/5" className={classes.slickLink} underline="none">
                                     <div><strong>{essentialRateList?.RET_DATA?.rate5?.score ? essentialRateList?.RET_DATA?.rate5?.score : "0%"}</strong></div>
                                     <div>업무수행 권한<br /> 및 책임</div>
                                 </Link>
                             </div>
                             <div className={classes.slickCircle + handleSlickCircleColor(essentialRateList?.RET_DATA?.rate6?.score)}>
-                                <Link to="#" onClick={() => DashboardLinkShow('6')} className={classes.slickLink} underline="none">
+                                <Link to="dashboard/employee/6" className={classes.slickLink} underline="none">
                                     <div><strong>{essentialRateList?.RET_DATA?.rate6?.score ? essentialRateList?.RET_DATA?.rate6?.score : "0%"}</strong></div>
                                     <div>안전보건 전문인력<br /> 배치</div>
                                 </Link>
                             </div>
                             <div className={classes.slickCircle + handleSlickCircleColor(essentialRateList?.RET_DATA?.rate7?.score)}>
-                                <Link to="#" onClick={() => DashboardLinkShow('7')} className={classes.slickLink} underline="none">
+                                <Link to="dashboard/employee/7" className={classes.slickLink} underline="none">
                                     <div><strong>{essentialRateList?.RET_DATA?.rate7?.score ? essentialRateList?.RET_DATA?.rate7?.score : "0%"}</strong></div>
                                     <div>종사자 개선<br /> 의견수렴</div>
                                 </Link>
                             </div>
                             <div className={classes.slickCircle + handleSlickCircleColor(essentialRateList?.RET_DATA?.rate8?.score)}>
-                                <Link to="#" onClick={() => DashboardLinkShow('8')} className={classes.slickLink} underline="none">
+                                <Link to="dashboard/employee/8" className={classes.slickLink} underline="none">
                                     <div><strong>{essentialRateList?.RET_DATA?.rate8?.score ? essentialRateList?.RET_DATA?.rate8?.score : "0%"}</strong></div>
                                     <div>비상대응<br /> 절차마련</div>
                                 </Link>
                             </div>
                             <div className={classes.slickCircle + handleSlickCircleColor(essentialRateList?.RET_DATA?.rate9?.score)}>
-                                <Link to="#" onClick={() => DashboardLinkShow('9')} className={classes.slickLink} underline="none">
+                                <Link to="dashboard/employee/9" className={classes.slickLink} underline="none">
                                     <div><strong>{essentialRateList?.RET_DATA?.rate9?.score ? essentialRateList?.RET_DATA?.rate9?.score : "0%"}</strong></div>
                                     <div>도급/용역 위탁 시<br /> 안전보건 확보</div>
                                 </Link>
@@ -988,7 +988,7 @@ const Director = () => {
                                 </Link>
                             </div>
                             <div className={classes.slickCircle + handleSlickCircleColor('0%')}>
-                                <Link to="#" onClick={() => DashboardLinkShow('12')} className={classes.slickLink} underline="none">
+                                <Link to="dashboard/employee/order-for-improvement-and-correction-under-related-law/list" className={classes.slickLink} underline="none">
                                     <div><strong>{relatedLawRate?.RET_DATA?.relatedLawRate ? relatedLawRate?.RET_DATA?.relatedLawRate : "0%"}</strong></div>
                                     <div>관계법령에 따른<br /> 의무이행</div>
                                 </Link>
